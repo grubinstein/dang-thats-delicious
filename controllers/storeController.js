42ae2cf8d74057ec78e8b92313480b9b2e54798f
Gabriel Rubinstein <gabrielrubinstein1@googlemail.com>
2019-10-28 10:17:07 +0000

    Added photo upload but it isn't saving

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+	storage: multer.memoryStorage(),
+	fileFilter(req, file, next) {
+		const isPhoto = file.mimetype.startsWith('image/');
+		if(isPhoto) {
+			next(null, true);
+		} else {
+			next({ message: 'That filetype isn\'t allowed!' }, false)
+		}
+	}
+}
+
 
 exports.homePage = (req, res) => {
     console.log(req.name);
@@ -9,6 +25,24 @@ exports.homePage = (req, res) => {
 exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store'});
 }
+
+exports.resize = async (req, res, next) => {
+	// check if there is no new file to resize
+	if(!req.file) {
+		next();
+		return
+	} 
+	const extension = req.file.mimetype.split('/')[1];
+	req.body.photo = `${uuid.v4()}.${extension}`;
+	// now we resize
+	const photo = await jimp.read(req.file.buffer);
+	await photo.resize(800, jimp.AUTO);
+	await photo.write(`./public/uploads/${req.body.photo}`)
+	// once we have written the photo to our filesystem, keep going
+	next();
+}
+
+exports.upload = multer(multerOptions).single('photo');
 
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
@@ -28,6 +62,8 @@ exports.editStore = async (req, res) => {
 }
 
 exports.updateStore = async (req, res) => {
+	// set the location data to be a point (it won't be when you edit it for some reason)
+	req.body.location.type = 'Point';
 	const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
 		new: true,
 		runValidators: true
