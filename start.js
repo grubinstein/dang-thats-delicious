98c6a56ab3e0165592f698357f6f51f9031434c1
Gabriel Rubinstein <gabrielrubinstein1@googlemail.com>
2019-11-02 12:50:36 +0000

    Started on register and login forms

--- start.js
+++ start.js
@@ -5,7 +5,7 @@ const reload = require('reload');
 require('dotenv').config({ path: 'variables.env' });
 
 // Connect to our Database and handle any bad connections
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
