8e51abaa9597c59804081460242c93936d78e8a4
Gabriel Rubinstein <gabrielrubinstein1@googlemail.com>
2019-11-04 10:22:28 +0000

    Completed login stuff

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 router.get('/', storeController.getStores);
 router.get('/stores', storeController.getStores);
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn,
+	storeController.addStore
+);
+
 router.post('/add', 
 	storeController.upload, 
 	catchErrors(storeController.resize), 
@@ -23,8 +27,15 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug))
 router.get('/tags', catchErrors(storeController.getStoresByTag))
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag))
 
-router.get('/login', userController.loginForm)
-router.get('/register', userController.registerForm)
-router.post('/register', userController.validateRegister);
+router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
+
+router.get('/register', userController.registerForm);
+router.post('/register', userController.validateRegister,
+	userController.register,
+	authController.login
+);
+
+router.get('/logout', authController.logout)
 
 module.exports = router;
